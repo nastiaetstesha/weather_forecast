70347f9f1bf7e1bab9fc8f30028ce97c841e4d51
Anastasiia <96624085+nastiaetstesha@users.noreply.github.com>
2024-12-09 22:16:11 +0200

    Update learn_request.py
    added m/c

--- learn_request.py
+++ learn_request.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     cities = ['london', 'svo', 'череповец']
 
     for city in cities:
-        url = f'https://wttr.in/{requests.utils.quote(city)}&lang=ru?3nT'
+        url = f'https://wttr.in/{requests.utils.quote(city)}&lang=ru?3nTM'
         response = requests.get(url)
         response.raise_for_status()
         print(response.text)
