95778eac166ad5931671b9786886782a24bfdc9d
Avakova Anastasiia <avakova.ast@gmail.com>
2024-12-09 22:08:59 +0200

    init commit

---
+++ learn_request.py
@@ -0,0 +1,11 @@
+import requests
+
+
+if __name__ == '__main__':
+    cities = ['london', 'svo', 'череповец']
+
+    for city in cities:
+        url = f'https://wttr.in/{requests.utils.quote(city)}&lang=ru?3nT'
+        response = requests.get(url)
+        response.raise_for_status()
+        print(response.text)
