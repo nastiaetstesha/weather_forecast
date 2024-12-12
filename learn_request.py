c8a35324516a1915df1db24a28cff9d86de4e67d
Avakova Anastasiia <avakova.ast@gmail.com>
2024-12-12 21:45:31 +0200

    hh

--- learn_request.py
+++ learn_request.py
@@ -1,11 +1,20 @@
 import requests
+from urllib.parse import urlencode
 
 
-if __name__ == '__main__':
-    cities = ['london', 'svo', 'череповец']
-
+def get_url():
     for city in cities:
+        base_url = f'https://wttr.in/{requests.utils.quote(city)}'
+        params = {
+            'lang': 'ru',
+            '3nTM': ''
+        }
+        response = requests.get(base_url, params=params)
         url = f'https://wttr.in/{requests.utils.quote(city)}&lang=ru?3nTM'
         response = requests.get(url)
         response.raise_for_status()
         print(response.text)
+
+if __name__ == '__main__':
+    cities = ['london', 'svo', 'череповец']
+    get_url()
