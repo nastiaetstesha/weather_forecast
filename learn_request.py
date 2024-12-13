91578d7110a57c446b4b93db99b894251beff088
Avakova Anastasiia <avakova.ast@gmail.com>
2024-12-13 21:42:10 +0200

    the name of func was changed

--- learn_request.py
+++ learn_request.py
@@ -1,20 +1,18 @@
 import requests
-from urllib.parse import urlencode
 
 
 cities = ['Лондон', 'Аэропорт шереметьево', 'Череповец']
 
-def get_url():
+def get_weather_for_cities():
     for city in cities:
         base_url = f'https://wttr.in/{city}'
         params = {
             'lang': 'ru',
             '3nTM': ''
         }
-        query_string = urlencode(params)  
         response = requests.get(base_url, params=params)
         response.raise_for_status()
         print(response.text)
 
 if __name__ == '__main__':
-    get_url()
+    get_weather_for_cities()
